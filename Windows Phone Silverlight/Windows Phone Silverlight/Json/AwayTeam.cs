6e1d0968aab7d9e831e9e44a3e4e49113004165f
Atte Söderlund <atte.a.soderlund@student.jyu.fi>
2014-07-21 17:44:39 +0300

    Added json converter
    Removed HttpClient.Compression and added instead Coding4Funs GZip
    compressions tool. Also dezerializes json, but do not show it yet.

---
+++ Windows Phone Silverlight/Windows Phone Silverlight/Json/AwayTeam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Windows_Phone_Silverlight.Json
+{
+    class AwayTeam
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Logo { get; set; }
+        public string LogoUrl { get; set; }
+        public int Ranking { get; set; }
+        public string Message { get; set; }
+    }
+}
--- Windows Phone Silverlight/Windows Phone Silverlight/MainPage.xaml.cs
+++ Windows Phone Silverlight/Windows Phone Silverlight/MainPage.xaml.cs
@@ -1,24 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Coding4Fun.Toolkit.Net;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Windows_Phone_Silverlight.Json;
 
 namespace Windows_Phone_Silverlight
 {
+    /// <summary>
+    /// Sovelluksen etusivu, johon tulee lista otteluista, jotka ladataan serveriltä.
+    /// </summary>
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
+        private List<OtteluRootObject> ottelut;
+        private Uri ottelutUrl = new Uri("http://adafyvlstorage.blob.core.windows.net/2014/finland/veikkausliiga/matches");
+
+        /// <summary>
+        /// Lataa suoraan ottelut serveriltä initializoituaan ensin komponentit.
+        /// </summary>
         public MainPage()
         {
             InitializeComponent();
+            LataaOttelut();
+        }
+
+        /// <summary>
+        /// Lataa ja purkaa gzipin serveriltä. Alustaa tapahtuma käsittelijän odottamaan, että lataus loppuu.
+        /// </summary>
+        private async void LataaOttelut()
+        {
+            Coding4Fun.Toolkit.Net.GzipWebClient gZipWebClient = new GzipWebClient();
+            gZipWebClient.DownloadStringAsync(ottelutUrl);
+            gZipWebClient.DownloadStringCompleted += gZipWebClient_DownloadStringCompleted;
+        }
+
+        /// <summary>
+        /// Kun otteluiden lataus saadaan suoritettua, tämä metodi herätetään ja se suorittaa Json
+        /// converttauksen, jos tulos ei ole null.
+        /// </summary>
+        void gZipWebClient_DownloadStringCompleted(object sender, System.Net.DownloadStringCompletedEventArgs e)
+        {
+            if (e.Result != null)
+                ottelut = JsonConvert.DeserializeObject<List<Json.OtteluRootObject>>(e.Result);
         }
     }
 }
