c85bd210c19744ce41bd5bcae83875dadfab83a2
Atte Söderlund <atte.a.soderlund@student.jyu.fi>
2014-07-22 13:23:40 +0300

    Now listbox is more clickable. Details added.
    In teleriks listbox you have to set background to transparent so it can
    be clicked. Also added try catch when connectin so it wont crash anymore
    if it cannot get connection.  Detailspage is now complete.

--- Windows Phone Silverlight/Windows Phone Silverlight/MainPage.xaml.cs
+++ Windows Phone Silverlight/Windows Phone Silverlight/MainPage.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.Phone.Controls;
-using Coding4Fun.Toolkit.Net;
 using Newtonsoft.Json;
 using Windows_Phone_Silverlight.Json;
 using System.Collections.ObjectModel;
-using System.Collections.Generic;
+using System.Net.Http;
+using AdvancedREI.Net.Http.Compression;
+using System.Diagnostics;
 
 namespace Windows_Phone_Silverlight
 {
@@ -37,31 +38,38 @@ namespace Windows_Phone_Silverlight
         }
 
         /// <summary>
-        /// Lataa ja purkaa gzipin serveriltä. Alustaa tapahtuma käsittelijän odottamaan, että lataus loppuu.
+        /// Lataa ja purkaa gzipin serveriltä. Kun otteluiden lataus saadaan suoritettua suoritetaan Json
+        /// converttauksen. Lopuksi lisätään ladatut ottelut matches ObservableCollectioniin,
+        /// jotta lista päivittyy ui:n puolella. Lisäksi otetaan latausikoni pois päältä.
         /// </summary>
-        private void DownloadMatches()
+        private async void DownloadMatches()
         {
-            Coding4Fun.Toolkit.Net.GzipWebClient gZipWebClient = new GzipWebClient();
-            gZipWebClient.DownloadStringAsync(ottelutUrl);
-            gZipWebClient.DownloadStringCompleted += GZipWebClientDownloadStringCompleted;
-        }
-
-        /// <summary>
-        /// Kun otteluiden lataus saadaan suoritettua, tämä metodi herätetään ja se suorittaa Json
-        /// converttauksen, jos tulos ei ole null. Lopuksi lisätään ladatut ottelut matches ObservableCollectioniin,
-        /// jotta lista päivittyy ui:n puolella.
-        /// </summary>
-        void GZipWebClientDownloadStringCompleted(object sender, System.Net.DownloadStringCompletedEventArgs e)
-        {
-            if (e.Error != null)
+            BusyIndicator.IsRunning = true;
+            HttpClient httpClient = new HttpClient(new CompressedHttpClientHandler());
+            httpClient.Timeout = TimeSpan.FromSeconds(10);
+            try
+            {
+                string result = await httpClient.GetStringAsync(ottelutUrl);
+                ObservableCollection<MatchRootObject> downloadedMatches = new ObservableCollection<MatchRootObject>
+                        (JsonConvert.DeserializeObject<ObservableCollection<MatchRootObject>>(result).OrderByDescending
+                        (x => x.MatchDate).Where(x => x.MatchDate <= DateTime.Now).ToArray()); // Järjestää niin että ylhäällä on uusimmat ja sitten ottaa vain ne, jotka on jo pelattu
+                foreach (MatchRootObject match in downloadedMatches)
+                    matches.Add(match);
+            }
+            catch(System.Net.Http.HttpRequestException)
             {
                 //log
-                return;
+                Debug.WriteLine("Ei internetyhteyttä");
+                Telerik.Windows.Controls.RadMessageBox.Show("Ei internetyhteyttä",Telerik.Windows.Controls.MessageBoxButtons.OK);
+                ReloadButton.Visibility = System.Windows.Visibility.Visible;
             }
-            List<Json.MatchRootObject> downloadedMatches = new List<Json.MatchRootObject>(JsonConvert.DeserializeObject<List<Json.MatchRootObject>>(e.Result).OrderByDescending
-                (x => x.MatchDate).Where(x => x.MatchDate <= DateTime.Now).ToArray());
-            foreach (MatchRootObject match in downloadedMatches)
-                matches.Add(match);
+            catch(Exception)
+            {
+                //log
+                Debug.WriteLine("Virhe yhteyttä muodostettaessa");
+                Telerik.Windows.Controls.RadMessageBox.Show("Virhe yhteyttä muodostettaessa", Telerik.Windows.Controls.MessageBoxButtons.OK);
+                ReloadButton.Visibility = System.Windows.Visibility.Visible;
+            }
             BusyIndicator.IsRunning = false;
         }
 
@@ -73,6 +81,15 @@ namespace Windows_Phone_Silverlight
         {
             NavigationService.Navigate(new Uri(matchDetailsPageUri + "?matchid=" + //lisätään match id, jotta ottelua voidaan hakea serveriltä.
                 ((Json.MatchRootObject)MatchesListBox.SelectedItem).Id.ToString() ,UriKind.Relative));
+        }
+
+        /// <summary>
+        /// Lataa matsit uudelleen, kun nappia painetaan.
+        /// </summary>
+        private void ReloadButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ReloadButton.Visibility = System.Windows.Visibility.Collapsed;
+            DownloadMatches();
         }
     }
 }
--- Windows Phone Silverlight/Windows Phone Silverlight/MatchDetailsPage.xaml.cs
+++ Windows Phone Silverlight/Windows Phone Silverlight/MatchDetailsPage.xaml.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Linq;
-using System.Net;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Coding4Fun.Toolkit.Net;
 using Newtonsoft.Json;
 using Windows_Phone_Silverlight.MatchDetailsJson;
-using System.Threading.Tasks;
-using System.Windows.Threading;
-using System.Diagnostics;
 using System.Net.Http;
 using AdvancedREI.Net.Http.Compression;
+using System.Diagnostics;
+using System.Windows.Media.Imaging;
 
 namespace Windows_Phone_Silverlight
 {
@@ -27,10 +24,42 @@ namespace Windows_Phone_Silverlight
 
         private async void DownloadMatchDetails()
         {
-            HttpClient httpClient = new HttpClient(new CompressedHttpClientHandler());
-            httpClient.Timeout = TimeSpan.FromSeconds(10);
-            string result = await httpClient.GetStringAsync(new Uri(detailsUrl + matchId));
-            details = JsonConvert.DeserializeObject<MatchDetailsRootObject>(result);
+            BusyIndicator.IsRunning = true;
+            try
+            {
+                HttpClient httpClient = new HttpClient(new CompressedHttpClientHandler());
+                httpClient.Timeout = TimeSpan.FromSeconds(10);
+                string result = await httpClient.GetStringAsync(new Uri(detailsUrl + matchId));
+                details = JsonConvert.DeserializeObject<MatchDetailsRootObject>(result);
+                SetDetails();
+            }
+            catch(System.Net.Http.HttpRequestException)
+            {
+                //log
+                Debug.WriteLine("Ei internetyhteyttä");
+                Telerik.Windows.Controls.RadMessageBox.Show("Ei internetyhteyttä", Telerik.Windows.Controls.MessageBoxButtons.OK);
+            }
+            catch (Exception)
+            {
+                //log
+                Debug.WriteLine("Virhe yhteyttä muodostettaessa");
+                Telerik.Windows.Controls.RadMessageBox.Show("Virhe yhteyttä muodostettaessa", Telerik.Windows.Controls.MessageBoxButtons.OK);
+            }
+            BusyIndicator.IsRunning = false;
+        }
+
+        /// <summary>
+        /// Asettaa joukkueiden logot, nimet, päivämäärän, tulksen ja ottelun tapahtumat.
+        /// </summary>
+        private void SetDetails()
+        {
+            HomeTeamLogo.Source = new BitmapImage(new Uri(details.Match.HomeTeam.LogoUrl));
+            AwayTeamLogo.Source = new BitmapImage(new Uri(details.Match.AwayTeam.LogoUrl));
+            HomeTeamNameTextBlock.Text = details.Match.HomeTeam.Name;
+            AwayTeamNameTextBlock.Text = details.Match.AwayTeam.Name;
+            ResultTextBlock.Text = details.Match.HomeGoals + " - " + details.Match.AwayGoals;
+            MatchActionsListBox.ItemsSource = details.MatchActions;
+            MatchDateTextBlock.Text = DateTime.Parse(details.Match.MatchDate).ToShortDateString();
         }
 
         /// <summary>
@@ -45,6 +74,15 @@ namespace Windows_Phone_Silverlight
                 DownloadMatchDetails();
             }
             base.OnNavigatedTo(e);
+        }
+
+        /// <summary>
+        /// Lataa matsit uudelleen, kun nappia painetaan.
+        /// </summary>
+        private void ReloadButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ReloadButton.Visibility = System.Windows.Visibility.Collapsed;
+            DownloadMatchDetails();
         }
     }
 }
